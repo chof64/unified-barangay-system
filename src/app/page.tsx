0709a8e92bccbd0bf0244f22d47a952d60b887de
Chad Fernandez <developer@chadfernandez.me>
2024-06-23 12:53:54 +0800

    feat: added home page and partial landing page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,55 @@
+import React from "react";
+import { Provider, Balancer } from "react-wrap-balancer";
 import Link from "next/link";
+import Image from "next/image";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+import { Button } from "~/components/ui/button";
+import { BorderBeam } from "~/components/magicui/border-beam";
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
-
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
+    <main className="mb-16">
+      <section className="bg-slate-50">
+        <div className="container pb-16 pt-32">
+          <Provider>
+            <h1 className="typography-h1 mx-auto max-w-4xl text-center">
+              <Balancer>Barangay documents right at your fingertips.</Balancer>
+            </h1>
+            <p className="typography-lead mx-auto mt-6 max-w-4xl text-center leading-8">
+              <Balancer>
+                The Unified Barangay System is a web-based application that
+                allows you to request your barangay documents online.
+              </Balancer>
             </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
+          </Provider>
+          <div className="mx-auto mt-6 flex w-fit gap-2">
+            <Button variant={"secondary"}>
+              <Link href="#learn">Learn More</Link>
+            </Button>
+            <Button>
+              <Link href="/request">Request Documents</Link>
+            </Button>
+          </div>
+          <div className="relative mt-8 aspect-[16/7.5] w-full rounded-2xl border">
+            <Image
+              className="rounded-2xl object-cover object-top"
+              src="/image/hero-image.png"
+              fill
+              alt="Screenshot of interface."
+            />
+            <BorderBeam size={250} duration={4} delay={9} />
           </div>
         </div>
-
-        <CrudShowcase />
-      </div>
+      </section>
+      <section className="mt-32">
+        <h2 className="typography-h2 border-none text-center">
+          Request your documents online.
+        </h2>
+        <p className="typography-lead mx-auto mt-4 max-w-3xl text-center">
+          Our goal is to make it easier for you to request your barangay
+          document. Now possible with the Unified Barangay System.
+        </p>
+      </section>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
