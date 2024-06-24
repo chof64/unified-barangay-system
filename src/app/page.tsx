8c64909602df7e3c917af6d429193f8524d5726a
Chad Fernandez <developer@chadfernandez.me>
2024-06-24 10:43:43 +0800

    refactor: updated hero image and minor style updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,14 @@ import Image from "next/image";
 
 import { Button } from "~/components/ui/button";
 import { BorderBeam } from "~/components/magicui/border-beam";
+import DotPattern from "~/components/magicui/dot-pattern";
 
 export default function Home() {
   return (
     <main className="mb-16">
-      <section className="bg-slate-50">
+      <section className="relative">
+        <DotPattern className="absolute -z-10 bg-slate-50 fill-neutral-300/60" />
+
         <div className="container pb-16 pt-32">
           <Provider>
             <h1 className="typography-h1 mx-auto max-w-4xl text-center">
@@ -23,25 +26,30 @@ export default function Home() {
             </p>
           </Provider>
           <div className="mx-auto mt-6 flex w-fit gap-2">
-            <Button variant={"secondary"}>
-              <Link href="#learn">Learn More</Link>
-            </Button>
             <Button>
               <Link href="/request">Request Documents</Link>
             </Button>
+            <Button variant={"secondary"}>
+              <Link href="#learn">Learn More</Link>
+            </Button>
           </div>
-          <div className="relative mt-8 aspect-[16/7.5] w-full rounded-2xl border">
+          <div className="relative mt-8 aspect-[16/7.5] w-full rounded-xl border shadow-lg">
             <Image
-              className="rounded-2xl object-cover object-top"
+              className="rounded-xl object-cover object-top"
               src="/image/hero-image.png"
               fill
               alt="Screenshot of interface."
             />
-            <BorderBeam size={250} duration={4} delay={9} />
+            <div className="absolute left-2.5 top-2.5 flex gap-1.5">
+              <div className="h-3 w-3 rounded-full bg-red-500" />
+              <div className="h-3 w-3 rounded-full bg-yellow-400" />
+              <div className="h-3 w-3 rounded-full bg-green-500" />
+            </div>
+            <BorderBeam size={250} duration={10} delay={9} />
           </div>
         </div>
       </section>
-      <section className="mt-32">
+      <section className="my-32">
         <h2 className="typography-h2 border-none text-center">
           Request your documents online.
         </h2>
