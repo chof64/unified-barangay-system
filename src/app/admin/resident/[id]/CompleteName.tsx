4e1cc62cf3f47aa4c87ec1f02c5cf31072e82dc9
Chad Fernandez <developer@chadfernandez.me>
2024-07-25 05:13:38 +0800

    refactor: remove unused file and some unused lines of code

--- src/app/admin/resident/[id]/CompleteName.tsx
+++ src/app/admin/resident/[id]/CompleteName.tsx
@@ -8,7 +8,6 @@ import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 import { type z } from "zod"
 
-import { cn } from "~/lib/utils"
 import { Button } from "~/components/ui/button"
 import {
   Form,
