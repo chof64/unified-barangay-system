cac9bfab9ed732c528a51f7e636a40669d2b8e08
Chad Fernandez <developer@chadfernandez.me>
2024-07-27 03:46:32 +0000

    chore: add devcontainer environment for development

---
+++ .devcontainer/install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+sudo apt install build-essential -y
+curl -fsSL https://git-town.com/install.sh | bash
+
+# Fix git town permissions
+chmod +x /home/node/.local/bin/git-town
+
+# Add git town as git aliases
+declare -A aliases=(
+    ["append"]="town append"
+    ["compress"]="town compress"
+    ["contribute"]="town contribute"
+    ["diff-parent"]="town diff-parent"
+    ["hack"]="town hack"
+    ["kill"]="town kill"
+    ["observe"]="town observe"
+    ["park"]="town park"
+    ["prepend"]="town prepend"
+    ["propose"]="town propose"
+    ["rename-branch"]="town rename-branch"
+    ["repo"]="town repo"
+    ["set-parent"]="town set-parent"
+    ["ship"]="town ship"
+    ["sync"]="town sync"
+)
+
+for alias in "${!aliases[@]}"; do
+    git config --global alias.$alias "${aliases[$alias]}"
+done
